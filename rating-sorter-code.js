453340fec5ec553ca1128000ebb5b740b6c7c93f
Hertz <43124833+hertz043@users.noreply.github.com>
2018-10-05 13:34:53 -0500

    code to sort recipe results into arrays by rating

---
+++ rating-sorter-code.js
@@ -0,0 +1,20 @@
+//Should go inside the AJAX call in order to access the proper variables
+
+for (i = 0; i < ratingObj.length; i++) {
+
+    if (ratingObj[i] >= useringredients.length) {
+        maxRatingRecipes.push(response.hits[i].recipe);
+    }
+
+    else if (ratingObj[i] === useringredients.length - 1) {
+        midRatingRecipes.push(response.hits[i].recipe);
+    }
+
+    else if (ratingObj[i] === useringredients.length - 2) {
+        lowRatingRecipes.push(response.hits[i].recipe);
+    }
+
+    else {
+        //psudeocode, need id for the DOM element that will display recipes
+        //"Sorry, we didn't find any recipes that matched closely enough with your ingredients."
+    }
