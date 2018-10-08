aa3025e83b557af5ab1f945f62f96fae6ed90555
kkasperek <kt.kasperek@gmail.com>
2018-10-08 12:44:59 -0500

    added cards to search-form.html

--- search-form/ingredient-tags.js
+++ search-form/ingredient-tags.js
@@ -8,9 +8,9 @@ $(document).ready(function () {
     console.log(ingredients);
     $('input.autocomplete').autocomplete({
         data,
-            // "Apple": null,
-            // "Watermellon": null,
-            // "Chicken": null
+        // "Apple": null,
+        // "Watermellon": null,
+        // "Chicken": null
         minLength: 3,
     });
 
@@ -38,34 +38,67 @@ $(document).ready(function () {
         // Loops through the array of topics
         for (i in userIngredients) {
 
-                // Then dynamicaly generates tags for each topic in the array
-                //<div class="chip">Chicken <i class="close material-icons">close</i></div>
-                let tag = $("<div>");                   //ingredient tag with class chip
-                tag.addClass("chip");
-                tag.html(userIngredients[i]);
+            // Then dynamicaly generates tags for each topic in the array
+            //<div class="chip">Chicken <i class="close material-icons">close</i></div>
+            let tag = $("<div>");                   //ingredient tag with class chip
+            tag.addClass("chip");
+            tag.html(userIngredients[i]);
 
-                let close = $("<i>");
-                close.addClass("close material-icons");
-                close.text("close");
-                close.val(userIngredients[i]);
+            let close = $("<i>");
+            close.addClass("close material-icons");
+            close.text("close");
+            close.val(userIngredients[i]);
 
-                $("#ingredientTags").append(tag);          // Added the button to the addTopics div
-                $(tag).append(close);
-            
+            $("#ingredientTags").append(tag);          // Added the button to the addTopics div
+            $(tag).append(close);
+
         };
     };
 
-    //on chip delete  
+    //on deleting ingredient tag 
     //1. removing button
     //2. remove from userIngredient array .splice
-    $(document).on("click", ".close", function(){
+    $(document).on("click", ".close", function () {
         var splicevalue = $(this).val();
-        
 
-        index= userIngredients.indexOf(splicevalue);
+
+        index = userIngredients.indexOf(splicevalue);
         userIngredients.splice(index, 1);
-        console.log(userIngredients); 
+        console.log(userIngredients);
     });
 
+    //Checkboxes --> add health and diet 
+
+    // var dietOptionsArray = [];
+    // var healthLabel = $(".health-label");
+    // console.log(healthLabel);
+
+
+    // $(".health-label").on("click", function () {
+    //     let gf = $("#gluten-free");
+    //     let pf = $("#peanut-free");
+    //     let lf = $("#low-fat");
+    //     let lc = $("#low-carb");
+    //     let ls = $("#low-sodium");
+    //     let p = $("#paleo");
+    //     let veg = $("#vegetarian");
+    //     let v = $("#vegan");
+
+    //     for (gf ) {
+    //         console.log('works');
+    //     }
+
+
+        // for (i in healthLabel) {
+        //     console.log(healthLabel[i].attributes.id.value);
+        //     var healthId = healthLabel[i].attributes.id.value;
+        // }
+        // if (dietOptionsArray.includes(healthLabel)) {
+        //     return 0;
+        // } else {
+        //     dietOptionsArray.push(healthId);
+        // }
+
+    // });
 
 });
